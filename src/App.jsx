ebfbcab82e1a9e4e3d174913cb7b33ffbff9df67
Keivi Veizan <kveizanp@est.emi.edu.bo>
2024-11-04 17:49:31 -0400

    Probando la rama veizan

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +6,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Cameo Puto</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          El segales no sabe hacer un clone <code>#QueAprendaLaSemanticaElSegales</code> Viva hitler
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Este es el primer commit de la rama Vei-Chan</h1>
     </>
   )
 }
