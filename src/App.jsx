ee871baedb4196fcd36e631bde92958a8494e76b
Keivi Veizan <kveizanp@est.emi.edu.bo>
2024-11-05 01:46:50 -0400

    Pantalla principal para dispositivos moviles

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,27 @@ function App() {
 
   return (
     <>
+    <article className='inicio'>
       <header>
-        <div className='header-top'>
-          <nav>
-            <ul>
-              <li><a href="">Nuestros Planes</a></li>
-              <li><a href="">Acerca de Nosotros</a></li>
-              <li><a href="">Inicio</a></li>
-            </ul>
-          </nav>
-        </div>
-        <div className='header-bottom'>
-          <h1>
-            Centro Ecuestre Apóstol Santiago S.A.
-          </h1>
-          <p>
-            Entrena, diviértete y vive experiencias inolvidables en nuestro campo ecuestre.
-          </p>
-        </div>
-      </header>
+          <div className='header-top'>
+            <nav>
+              <ul>
+                <li><a href="">Nuestros Planes</a></li>
+                <li><a href="">Acerca de Nosotros</a></li>
+                <li><a href="">Inicio</a></li>
+              </ul>
+            </nav>
+          </div>
+        </header>
+          <div className='header-bottom'>
+            <h1>
+              Centro Ecuestre Apóstol Santiago S.A.
+            </h1>
+            <p>
+              Entrena, diviértete y vive experiencias inolvidables en nuestro campo ecuestre.
+            </p>
+          </div>
+    </article>      
     </>
   )
 }
