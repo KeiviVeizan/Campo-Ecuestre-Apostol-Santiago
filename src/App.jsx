aa3fe3745b75c7bfd0a7c96c88f0f60c63f318a8
Keivi Veizan <kveizanp@est.emi.edu.bo>
2024-11-12 08:11:44 -0400

    Fumandas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
 import './App.css'
-
+import './Components/sign_in'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Este es el primer commit de la rama Vei-Chan</h1>
-    </>
+    <>{Sign_in}</>
   )
 }
 
---
+++ src/Components/sign_in.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Sign_in() {
+    const Sexovega = {
+
+    }
+  return (
+    <main>
+        <input type="text" placeholder={description}/>
+        <input type="password" placeholder={description}/>
+    </main>
+  )
+}
+
+export default Sign_in
