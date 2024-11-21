9cdc0fc5f7458183fcc1bd765f3add0dce108fac
Keivi Veizan <kveizanp@est.emi.edu.bo>
2024-11-21 01:00:18 -0400

    Arreglando responsive

---
+++ src/Components/Section4.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../component css/PreguntasFrecuentes.css'
+export const Section4 = () => {
+  return (
+    <article className='section4-info'>
+        <h3>
+            Preguntas Frecuentes
+        </h3>
+    </article>
+  )
+}
