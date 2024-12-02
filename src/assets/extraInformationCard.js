2ea5f8cc91af357c5328254da8476c355b01ee18
Keivi Veizan <kveizanp@est.emi.edu.bo>
2024-12-01 23:49:30 -0400

    Comienzo de animaciones

--- src/assets/extraInformationCard.js
+++ src/assets/extraInformationCard.js
@@ -1,18 +1,18 @@
 const extraInformationCard = [{
     id:1,
-    description: "el centro ecuestre apóstol santiago s.a. es una sociedad anónima concebida por la iniciativa de un grupo de oficiales reunidos y unidos por el amor al caballo, que creyó firmemente en la consolidación de una sociedad de carácter deportivo social y cultural que principalmente fomenta la práctica de los deportes ecuestres a través de: la enseñanza, organización y participación en eventos locales nacionales e internacionales ​promoviendo y desarrollando entre sus asociados actividades de carácter social, cultural y deportivo enmarcados en principios de hermandad solidaridad y principalmente en el respeto que cada ser humano debe tener con sus semejantes​",
+    description: "El Centro Ecuestre Apóstol Santiago S.A. es una sociedad anónima fundada por un grupo de oficiales comprometidos con la promoción de los deportes ecuestres, dedicada a la enseñanza, organización de eventos locales e internacionales, y actividades sociales, culturales y deportivas, destacando valores de hermandad, solidaridad y respeto.",
     images: "/public/images/img1-page1.avif"
 },
 {
     id:2,
     title: "El Campo Ecuestre Apostol Santiago es un proyecto a futuro",
-    description:"El proyecto del Centro Ecuestre Apóstol Santiago S.A., con un lanzamiento previsto para 2025, es una experiencia única en su clase, diseñada para toda la familia y centrada en ofrecer instalaciones de primera calidad. Nuestro enfoque principal es la equitación, pero va mucho más allá de los clubes tradicionales. Contará con amplias áreas deportivas, instalaciones de primer nivel para eventos, un spa de relajación, y espacios sociales que fomentan la convivencia y el bienestar de toda la familia. Este espacio no solo invita a la práctica ecuestre en un ambiente de excelencia, sino que integra actividades y comodidades que aseguran una experiencia integral, atractiva y completa para los socios e inversionistas que busquen un club moderno, exclusivo y con un enfoque innovador en la ciudad.",
+    description:"El Centro Ecuestre Apóstol Santiago S.A., previsto para 2025, ofrecerá una experiencia única y moderna para toda la familia, combinando instalaciones de excelencia con un enfoque innovador. Más allá de la equitación, contará con áreas deportivas, un spa de relajación, espacios sociales y eventos de primer nivel. Diseñado para fomentar la convivencia y el bienestar, será un club exclusivo que garantiza una experiencia integral para socios e inversionistas, redefiniendo los estándares de los clubes tradicionales en la ciudad.",
     images:"/public/images/img2-page1.webp"
 },
 {
     id:3, 
     title:"¿Porque invertir en el campo ecuestre Apostol santiago?",
-    description:"Invertir en nuestro campo ecuestre es una oportunidad única para formar parte de un proyecto que combina deporte, naturaleza y recreación en un entorno de alto nivel. Nuestro campo no solo ofrece la posibilidad de practicar equitación, sino también una amplia gama de actividades deportivas y recreativas que atraen a familias, deportistas y amantes del aire libre. Al ser un espacio integral con áreas de entrenamiento, comedor y actividades para todas las edades, garantiza un flujo constante de visitantes que buscan una experiencia completa, lo que convierte la inversión en una opción rentable y de crecimiento sostenible.",
+    description:"Invertir en nuestro campo ecuestre es una oportunidad excepcional para ser parte de un proyecto que fusiona deporte, naturaleza y recreación en un entorno exclusivo. Más allá de la equitación, ofrece actividades deportivas y recreativas para todas las edades, atrayendo a familias y amantes del aire libre. Con áreas de entrenamiento, comedor y espacios integrales, asegura un flujo constante de visitantes, convirtiéndose en una inversión rentable y sostenible.",
     images:"/public/images/img3-page1.jpg"
 }
 ];
